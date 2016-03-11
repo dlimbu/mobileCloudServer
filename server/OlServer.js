b4a152af26d6e6e311fb570d35df652b1e68d8cb
dlimbu <dipu.limbu@twcable.com>
2016-03-10 17:44:34 -0700

    basic node

---
+++ server/OlServer.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Dlimbu
+ */
+
+var express = require('express');
+
+var OlServer = function (options) {
+   this._sInst = express();
+};
+
+OlServer.prototype.get = function (resource) {
+   this._sInst.get(resource, function (req, res) {
+      res.writeHead(200, {
+         'content-Type' : 'text/plain'
+      });
+      res.end('Welcome to Offload Server!');
+
+   });
+};
+
+/**
+ *
+ * @param resource
+ */
+OlServer.prototype.post = function (resource) {
+   this._sInst.post(resource, function (req, res) {
+      res.send('Got a POST request');
+   });
+};
+
+/**
+ * Start the process on port.
+ * @param port
+ */
+OlServer.prototype.start = function (port) {
+   this._sInst.listen(port, function () {
+      console.log("OffLoadServer started...");
+   });
+};
+
+/**
+ * Stop the process/
+ */
+OlServer.prototype.stop = function () {
+   this._sInst.stop();
+};
+
+exports.OlServer = OlServer;
---
+++ server/appService.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dlimbu on 3/10/16.
+ */
+
+var OlServer = require('./OlServer').OlServer;
+
+var olServer = new OlServer();
+olServer.start(8080);
+
